4f829e273a1bb846030e7a8900eb0b2171aae220
Project-Name441 <36046509+Project-Name441@users.noreply.github.com>
2018-03-08 17:37:02 -0800

    Add files via upload
    Forgot to add some other stuff we need, please let me know what else is missing
    -Category.cpp and .h
    -transaction.cpp and.h 
    are included

--- category.cpp
+++ category.cpp
@@ -1,10 +1,21 @@
 #include <QString>
 #include "category.h"
+
 Category::Category()
 {
 name = "";
 }
 
+/* PURPOSE: It will create a temporary object of type Transaction,
+ * set its attributes using the passed parameters, and then add that temporary object
+ * to the Transaction vector.
+ *
+ * PARAMETERS: category is the category name that the transaction belongs to
+ *             name is the name of the transaction
+ *             date is the date that the transaction was made
+ *             amount is the amount of the transaction
+ *             type is a flag that determines whether it is an expense or income
+ * */
 void Category::addTransaction(QString category, QString name, QString date, double amount, QString type)
 {
     Transaction temp;
@@ -16,11 +27,16 @@ void Category::addTransaction(QString category, QString name, QString date, doub
     Transactions.push_back(temp);
 }
 
-//gets the total amount from all transactions
+/*PURPOSE: It will add up the amounts of every transaction and return it.
+ * Author: Jose Quirarte
+ * Date: 3/6/18
+ *
+ * */
 int Category::totalTransactions() const
 {
-    int total = 0;
-    for(int i = 0; (unsigned)i < Transactions.size(); i++)
+    int   total = 0;
+
+    for(int i = 0; (unsigned) i < Transactions.size(); i++)
         total += Transactions[i].getTransactionAmount();
 
     return total;
@@ -31,7 +47,18 @@ void Category::setCategoryName(QString s)
     name = s;
 }
 
+
 QString Category::getCategoryName() const
 {
     return name;
+}
+
+
+//PURPOSE: It will remove a transaction from the Transactions vector at the passed index
+//PARAMETER: index will be used to remove a transaction at a specific spot in the vector
+//Author: Jose Quirarte
+//Date: 3/7/18
+void Category::removeTransaction(int index)
+{
+    Transactions.erase(Transactions.begin() + index);
 }
--- category.h
+++ category.h
@@ -1,3 +1,13 @@
+/*Category will have a name specified by the user and will contain a vector
+ * full of transactions that belong to a specific category
+ * change to what Team wants
+ *
+ * Author: Jose Quirarte
+ * Date: 3/5/18
+ *
+ * */
+
+
 #ifndef CATEGORY_H
 #define CATEGORY_H
 #include <vector>
@@ -9,7 +19,7 @@ using namespace std;
 class Category
 {
 private:
-    QString name;                    //name of category
+    QString name;                     //name of category
     vector<Transaction> Transactions;
 
 public:
@@ -17,9 +27,9 @@ public:
     void setCategoryName(QString s);
     QString getCategoryName() const;
     void addTransaction(QString category, QString name, QString date, double amount, QString type);
-    //void editTransaction(); will be done later...ask about how edit will know what transaction to edit
-    //void removeTransaction(); will be done later...ask about how remove will know what transaction to remove
+    //void editTransaction(int index);  talk to group about how to implement this function
+    void removeTransaction(int index);     //removes a transaction from the category
     int totalTransactions() const;           //adds up all of the amounts of every transaction in one category
 };
 
-#endif // SPENDINGS_H
+#endif // CATEGORY_H
