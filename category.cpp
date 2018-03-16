ecfc779136b6f94bb32866c4a184fb442857537f
Project-Name441 <36046509+Project-Name441@users.noreply.github.com>
2018-03-15 18:26:15 -0700

    Add files via upload
    fixed an issue where I was setting a variable with a parameter with the same name

--- category.cpp
+++ category.cpp
@@ -42,9 +42,9 @@ int Category::totalTransactions() const
     return total;
 }
 
-void Category::setCategoryName(QString name)
+void Category::setCategoryName(QString Name)
 {
-    name = name;
+    name = Name;
 }
 
 
